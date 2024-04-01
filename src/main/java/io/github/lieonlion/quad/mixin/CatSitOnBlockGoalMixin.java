1b46945a40ed579e936128bcd69e6d06e89cdbdd
lieonlion <lieonlion4@gmail.com>
2024-04-01 18:21:16 +0100

    1.1.4

--- src/main/java/io/github/lieonlion/quad/mixin/CatSitOnBlockGoalMixin.java
+++ src/main/java/io/github/lieonlion/quad/mixin/CatSitOnBlockGoalMixin.java
@@ -1,9 +1,12 @@
 package io.github.lieonlion.quad.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.github.lieonlion.quad.tags.QuadBlockTags;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.ai.goal.CatSitOnBlockGoal;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ChestBlock;
 import net.minecraft.world.level.block.FurnaceBlock;
@@ -15,13 +18,20 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class CatSitOnBlockGoalMixin {
     @WrapOperation(method = "isValidTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 0))
     private boolean applyCatSitOnChestTag(BlockState instance, Block block, Operation<Boolean> original) {
-        return (original.call(instance, block) && block.builtInRegistryHolder().is(QuadBlockTags.CAT_SIT_ON_CHEST))
-                || (instance.getBlock() instanceof ChestBlock && instance.is(QuadBlockTags.CAT_SIT_ON_CHEST));
+        return (original.call(instance, block) && block.builtInRegistryHolder().is(QuadBlockTags.CAT_SIT_ON_BLOCK))
+                || (instance.getBlock() instanceof ChestBlock && instance.is(QuadBlockTags.CAT_SIT_ON_BLOCK));
     }
 
     @WrapOperation(method = "isValidTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 1))
     private boolean applyCatSitOnFurnaceTag(BlockState instance, Block block, Operation<Boolean> original) {
-        return (original.call(instance, block) && block.builtInRegistryHolder().is(QuadBlockTags.CAT_SIT_ON_FURNACE))
-                || (instance.getBlock() instanceof FurnaceBlock && instance.is(QuadBlockTags.CAT_SIT_ON_FURNACE));
+        return (original.call(instance, block) && block.builtInRegistryHolder().is(QuadBlockTags.CAT_SIT_ON_BLOCK))
+                || (instance.getBlock() instanceof FurnaceBlock && instance.is(QuadBlockTags.CAT_SIT_ON_BLOCK));
+    }
+
+    @ModifyReturnValue(method = "isValidTarget", at = @At(value = "RETURN"))
+    private boolean applyCatSitOnBlockTag(boolean original, LevelReader level, BlockPos pos) {
+        BlockState blockstate = level.getBlockState(pos);
+        return original || (level.isEmptyBlock(pos.above()) && blockstate.is(QuadBlockTags.CAT_SIT_ON_BLOCK)
+                && !(blockstate.getBlock() instanceof FurnaceBlock) && !(blockstate.getBlock() instanceof ChestBlock));
     }
 }
--- src/main/java/io/github/lieonlion/quad/tags/QuadBlockTags.java
+++ src/main/java/io/github/lieonlion/quad/tags/QuadBlockTags.java
@@ -7,8 +7,8 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class QuadBlockTags {
-    public static final TagKey<Block> CAT_SIT_ON_CHEST = createTag("cat_sit_on_chest");
-    public static final TagKey<Block> CAT_SIT_ON_FURNACE = createTag("cat_sit_on_furnace");
+    public static final TagKey<Block> CAT_SIT_ON_BLOCK = createTag("cat_sit_on_block");
+    public static final TagKey<Block> CAT_LIE_ON_BLOCK = createTag("cat_lie_on_block");
 
     private static TagKey<Block> createTag(String name) {
         return TagKey.create(Registries.BLOCK, new ResourceLocation(Quad.MODID, name));
