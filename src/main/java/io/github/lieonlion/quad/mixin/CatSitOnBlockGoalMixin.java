73ce6b42a069c1123ab2ca2e341c9706cc395365
lieonlion <lieonlion4@gmail.com>
2024-04-01 18:21:08 +0100

    1.1.4

--- src/main/java/io/github/lieonlion/quad/mixin/CatSitOnBlockGoalMixin.java
+++ src/main/java/io/github/lieonlion/quad/mixin/CatSitOnBlockGoalMixin.java
@@ -1,5 +1,6 @@
 package io.github.lieonlion.quad.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.github.lieonlion.quad.tags.QuadBlockTags;
@@ -8,6 +9,8 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.ChestBlock;
 import net.minecraft.block.FurnaceBlock;
 import net.minecraft.entity.ai.goal.CatSitOnBlockGoal;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -15,13 +18,20 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class CatSitOnBlockGoalMixin {
     @WrapOperation(method = "isTargetPos", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z", ordinal = 0))
     private boolean applyCatSitOnChestTag(BlockState instance, Block block, Operation<Boolean> original) {
-        return (original.call(instance, block) && block.getRegistryEntry().isIn(QuadBlockTags.CAT_SIT_ON_CHEST))
-                || (instance.getBlock() instanceof ChestBlock && instance.isIn(QuadBlockTags.CAT_SIT_ON_CHEST));
+        return (original.call(instance, block) && block.getRegistryEntry().isIn(QuadBlockTags.CAT_SIT_ON_BLOCK))
+                || (instance.getBlock() instanceof ChestBlock && instance.isIn(QuadBlockTags.CAT_SIT_ON_BLOCK));
     }
 
     @WrapOperation(method = "isTargetPos", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z", ordinal = 1))
     private boolean applyCatSitOnFurnaceTag(BlockState instance, Block block, Operation<Boolean> original) {
-        return (original.call(instance, block) && block.getRegistryEntry().isIn(QuadBlockTags.CAT_SIT_ON_FURNACE))
-                || (instance.getBlock() instanceof FurnaceBlock && instance.isIn(QuadBlockTags.CAT_SIT_ON_FURNACE));
+        return (original.call(instance, block) && block.getRegistryEntry().isIn(QuadBlockTags.CAT_SIT_ON_BLOCK))
+                || (instance.getBlock() instanceof FurnaceBlock && instance.isIn(QuadBlockTags.CAT_SIT_ON_BLOCK));
+    }
+
+    @ModifyReturnValue(method = "isTargetPos", at = @At(value = "RETURN"))
+    private boolean applyCatSitOnBlockTag(boolean original, WorldView world, BlockPos pos) {
+        BlockState blockstate = world.getBlockState(pos);
+        return original || (world.isAir(pos.up()) && blockstate.isIn(QuadBlockTags.CAT_SIT_ON_BLOCK)
+                && !(blockstate.getBlock() instanceof FurnaceBlock) && !(blockstate.getBlock() instanceof ChestBlock));
     }
 }
